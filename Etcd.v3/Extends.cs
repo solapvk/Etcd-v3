4bf727ec4fdbff70684bcebce609c1ace629c9b3
solapvk <solapvk@hotmail.com>
2017-09-11 14:52:33 +0800

    com

---
+++ Etcd.v3/Extends.cs
@@ -0,0 +1,25 @@
+﻿namespace Etcd.v3
+{
+    public static class Extends
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static Google.Protobuf.ByteString WithPrefix(this Google.Protobuf.ByteString str)
+        {
+            var end = str.ToByteArray();
+
+            for (int i = end.Length - 1; i >= 0; i--)
+            {
+                if (end[i] < 0xff)
+                {
+                    end[i] = (byte)(end[i] + 1);
+                    return Google.Protobuf.ByteString.CopyFrom(end);
+                }
+            }
+            return Google.Protobuf.ByteString.CopyFrom(end);
+        }
+    }
+}
